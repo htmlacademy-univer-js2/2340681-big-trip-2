b31442ef781879dfa9da829a2f4bad3ffd429b63
Robert <rozenkov03@mail.ru>
2023-06-17 02:21:50 +0500

    Поправил презентер

--- src/presenter/presenter.js
+++ src/presenter/presenter.js
@@ -1,9 +1,9 @@
-import NewCreateFormView from '../view/create-form';
-import NewEditFormView from '../view/edit-form';
-import NewFilterView from '../view/filters';
-import NewSortView from '../view/sort';
-import NewPointTripView from '../view/point-trip';
-import { render } from '../render.js';
+import NewCreateFormView from '../view/create-form.js';
+import NewEditFormView from '../view/edit-form.js';
+import NewFilterView from '../view/filters.js';
+import NewSortView from '../view/sort.js';
+import NewPointTripView from '../view/point-trip.js';
+import { render, RenderPosition } from '../render.js';
 
 export default class TripPresenter {
     filtersView = new NewFilterView();
